99421be7f9b18dad5f1241cc06f9dc03d0c07300
enkster-train <steven_enk@hotmail.com>
2026-02-10 23:02:28 -0500

    Update streamlit_app.py
    fatfinger

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/")
-        st.text(smoothuefroot_response.json())
+        st.text(smoothiefroot_response.json())
         # sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
