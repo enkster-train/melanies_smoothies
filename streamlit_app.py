796fce4fdce7b9969a29594809c2e68aecb06f5f
enkster-train <steven_enk@hotmail.com>
2026-02-11 00:12:19 -0500

    Update streamlit_app.py
    updated to use location values

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,12 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
+
 name_on_order = st.text_input("Name on Smoothie")
 
-st.write("The name on the smoothie will be: ", name_on_order);
+#st.write("The name on the smoothie will be: ", name_on_order);
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -36,6 +37,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')      
+      
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search)
         #st.text(smoothiefroot_response.json())
